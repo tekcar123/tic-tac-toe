213e589e9cdfdaab869d821eeed0ee7b92586f08
tekcar123 <derekool@icloud.com>
2022-08-08 14:26:20 -0400

    Remove unnecessary lines

--- game.py
+++ game.py
@@ -9,44 +9,24 @@ def print_game(game):
 
 def play(player):
     command = input(f"{player}'s turn where do you want to play? ")
-    if player == "X":
-        if command == "top-left":
-            game[0] = "X"
-        elif command == "top":
-            game[1] = "X"
-        elif command == "top-right":
-            game[2] = "X"
-        elif command == "left":
-            game[3] = "X"
-        elif command == "center":
-            game[4] = "X"
-        elif command == "right":
-            game[5] = "X"
-        elif command == "bottom-left":
-            game[6] = "X"
-        elif command == "bottom":
-            game[7] = "X"
-        elif command == "bottom-right":
-            game[8] = "X"
-    elif player == "O":
-        if command == "top-left":
-            game[0] = "O"
-        elif command == "top":
-            game[1] = "O"
-        elif command == "top-right":
-            game[2] = "O"
-        elif command == "left":
-            game[3] = "O"
-        elif command == "center":
-            game[4] = "O"
-        elif command == "right":
-            game[5] = "O"
-        elif command == "bottom-left":
-            game[6] = "O"
-        elif command == "bottom":
-            game[7] = "O"
-        elif command == "bottom-right":
-            game[8] = "O"
+    if command == "top-left":
+        game[0] = player
+    elif command == "top":
+        game[1] = player
+    elif command == "top-right":
+        game[2] = player
+    elif command == "left":
+        game[3] = player
+    elif command == "center":
+        game[4] = player
+    elif command == "right":
+        game[5] = player
+    elif command == "bottom-left":
+        game[6] = player
+    elif command == "bottom":
+        game[7] = player
+    elif command == "bottom-right":
+        game[8] = player
 
 
 while game_finished == False:
